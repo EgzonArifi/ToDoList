035331eaf92d684021dbeecc1dc22327d84bb3f0
Egzon's Macbook <egzonarifi@live.com>
2017-01-28 19:38:38 +0100

    some routes added

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -9,4 +9,19 @@ drop.get("index") { request in
     return try drop.view.make("index",["message":"Hello World"])
 }
 
+drop.get("milk", Int.self, String.self) { request, liters, person in
+    return try JSON (node: [
+        "message": "Take one downd, pass it around, \(liters - 10) bootles of milk on the wall",
+        "name": "Milk from \(person)"])
+}
+
+drop.post("post") { request in
+
+    guard let name = request.data["name"]?.string else{
+        throw Abort.badRequest
+    }
+    return try JSON(node: ["message": "Helloo \(name)"])
+
+}
+
 drop.run()
